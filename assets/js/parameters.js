846752c9417087e0b8344c6571a322fa2ae15075
JLP-55 <jack.v.pekin@gmail.com>
2024-01-17 11:37:49 +1100

    Dynamic buttons generated for the user input values that aren't boolean Changes to the parameters js file Basically just created more for loops to iterate through the arrays added to parameters js file and add further values to api call See the comments at the beginning of each function as they will describe what is going on

--- assets/js/parameters.js
+++ assets/js/parameters.js
@@ -9,24 +9,50 @@ var parametersArray = [{
     content: ["Indoor"],
 },];
 
-// var edible = "&edible=1";
-// var poisonous = "&poisonous=1";
-// var indoor = "&indoor=1";
-// var sunlight = [
-//     "&sunlight=full_sun",
-//     "&sunlight=sun-part_shade",
-//     "&sunlight=part-shade",
-//     "&sunlight=full_shade"
-// ];
-// var cycle = [
-//     "&cycle=annual",
-//     "&cycle=biannual",
-//     "&cycle=perennial",
-//     "&cycle=biennial"
-// ];
-// var watering = [
-//     "&waterning=frequent",
-//     "&waterning=average",
-//     "&waterning=minimum",
-//     "&waterning=none"
-// ];
+// Relevant to the dynamically generated buttons
+var parametersArraySunlight = [{
+    addToParameters: [
+        "&sunlight=full_sun",
+        "&sunlight=sun-part_shade",
+        "&sunlight=part-shade",
+        "&sunlight=full_shade",
+    ],
+}, {
+    content: ["Sunlight"],
+}, {
+    content: ["Sunlight"],
+}, {
+    contentPlus: ["Full sun", "Medium to full sun", "Part shade", "Full shade" ],
+}];
+
+// Relevant to the dynamically generated buttons
+var parametersArrayCycle = [{
+    addToParameters: [
+        "&cycle=annual",
+        "&cycle=biannual",
+        "&cycle=perennial",
+        "&cycle=biennial",
+    ],
+}, {
+    content: ["Cycle"],
+}, {
+    content: ["Cycle"],
+}, {
+    contentPlus: ["annual", "biannual", "perennial", "biennial",]
+}];
+
+// Relevant to the dynamically generated buttons
+var parametersArrayWatering = [{
+    addToParameters: [
+        "&watering=frequent",
+        "&watering=average",
+        "&watering=minimum",
+        "&watering=none",
+    ],
+}, {
+    content: ["Watering"],
+}, {
+    content: ["Watering"],
+}, {
+    contentPlus: ["Frequent", "Average", "Minimum", "None",],
+}];
